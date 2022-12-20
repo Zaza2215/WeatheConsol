1626d8c980de037af20de8b5837c52cfdcc17704
Pasha Kozachenko <pasha22151521@gmail.com>
2022-12-20 19:51:29 +0200

    Weather with lang and params
    * Change language
    * Output weather for San Francisco, Chrepovets, Sheremetyevo airoport

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# Start project Weather Console
+import requests
+
+places = {
+    'San Francisco': 'san%20francisco',
+    'Cherepovets': 'cherepovets',
+    'Sheremetyevo airoport': 'SVO',
+}
+
+# if you want to set russian language, replace value 'en' to 'ru'
+lang = 'en'
+
+url = 'https://wttr.in/{value}?nTmq&lang={lang}'
+
+for key, value in places.items():
+    response = requests.get(url.format(value=value, lang=lang))
+    response.raise_for_status()
+    print(response.text.replace(value, key))
